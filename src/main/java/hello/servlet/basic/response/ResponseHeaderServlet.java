26d7dbccbe0a1ab6a37be9f7f2f8a3897ba28ed8
Park Jung-Hwan <wjdghks5698@gmail.com>
2024-12-09 23:29:00 +0900

    섹션 3. 서블릿 - HTTP 요청 데이터(쿼리 파라미터, Form, Message Body), HttpServletResonse 사용법

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,64 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //[status-line]
+        response.setStatus(HttpServletResponse.SC_OK);//200 ok
+
+        //[response-headers]
+//        response.setHeader("Content-Type", "text/plain;charset=utf-8"); //기본 텍스트 형식
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); //캐시를 완전히 무효화
+        response.setHeader("Pragma", "no-cache"); //과거버전까지 다 캐시를 무효화
+        response.setHeader("my-header", "hello");
+
+        //[Header 편의 메서드]
+//        content(response);
+//        cookie(response);
+        redirect(response);
+
+
+        //[message body]
+        response.getWriter().println("ok");
+
+    }
+
+    //Content 편의 메서드
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //response.setContentLength(2); //(생략시 자동 생성)
+    }
+
+    //Cookie 편의 메서드
+    private void cookie(HttpServletResponse response) {
+        //Set-Cookie: myCookie=good; Max-Age=600;
+        //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
+    }
+
+    //Redirect 편의 메서드
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+//        response.setStatus(HttpServletResponse.SC_FOUND); //302
+//        response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
+    }
+}
